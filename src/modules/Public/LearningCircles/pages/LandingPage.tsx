163ad03055cbb5b73f73ca90b4d6897837130ba5
Jenin82 <jenin8282@gmail.com>
2023-12-28 08:29:51 +0530

    refactor(lc): public page components made

--- src/modules/Public/LearningCircles/pages/LandingPage.tsx
+++ src/modules/Public/LearningCircles/pages/LandingPage.tsx
@@ -217,45 +217,7 @@ const LandingPage = () => {
 
     return (
         <div className={styles.LClandingPage}>
-            <nav className={styles.LClandingPageNav}>
-                <img src="https://i.ibb.co/vY786NX/image.png" alt="muLearn" />
-                <div className={styles.navLinks}>
-                    <div>
-                        <Link to="https://mulearn.org/">About</Link>
-                        <Link to="https://mulearn.org/events/">Programs</Link>
-                        <Link to="https://learn.mulearn.org/">Interest Group</Link>
-                        <Link to="https://mulearn.org/careers">Careers</Link>
-                    </div>
-                    <button
-                        onClick={() => {
-                            navigate("/dashboard/connect-discord");
-                        }}
-                    >
-                        Join Us
-                    </button>
-                </div>
-            </nav>
-
-            <div className={styles.LClandingPageHero}>
-                <div className={styles.dash}></div>
-                <div className={styles.heroTitle}>
-                    <span>
-                        <b>Introducing</b>{" "}
-                        <img src="https://i.ibb.co/FDQ2M4n/Learn.png" alt="" />
-                    </span>
-                    <b>Learning Circles</b>
-                </div>
-                <p>
-                    An informal mechanism for bringing together learners who are
-                    interested in the same topic from across different fields
-                    and disciplines. A fantastic way to spend a small amount of
-                    time learning about new things with a group of people with
-                    same interests!
-                </p>
-                <button onClick={() => navigate("/dashboard/learning-circle")}>
-                    Create/Join Learning Circles
-                </button>
-            </div>
+            
 
             <div className={styles.LClandingPageEarth}>
                 <div className={styles.totalCount}>
---
+++ src/modules/Public/LearningCircles/pages/components/LcHero.tsx
@@ -0,0 +1,55 @@
+import { Link, useNavigate } from "react-router-dom";
+import styles from "../LandingPage.module.css";
+
+type Props = {};
+
+const LcHero = (props: Props) => {
+	const navigate = useNavigate();
+    return (
+        <>
+            <nav className={styles.LClandingPageNav}>
+                <img src="https://i.ibb.co/vY786NX/image.png" alt="muLearn" />
+                <div className={styles.navLinks}>
+                    <div>
+                        <Link to="https://mulearn.org/">About</Link>
+                        <Link to="https://mulearn.org/events/">Programs</Link>
+                        <Link to="https://learn.mulearn.org/">
+                            Interest Group
+                        </Link>
+                        <Link to="https://mulearn.org/careers">Careers</Link>
+                    </div>
+                    <button
+                        onClick={() => {
+                            navigate("/dashboard/connect-discord");
+                        }}
+                    >
+                        Join Us
+                    </button>
+                </div>
+            </nav>
+            <div className={styles.LClandingPageHero}>
+                <div className={styles.dash}></div>
+                <div className={styles.heroTitle}>
+                    <span>
+                        <b>Introducing</b>{" "}
+                        <img src="https://i.ibb.co/FDQ2M4n/Learn.png" alt="" />
+                    </span>
+                    <b>Learning Circles</b>
+                </div>
+                <p>
+                    An informal mechanism for bringing together learners who are
+                    interested in the same topic from across different fields
+                    and disciplines. A fantastic way to spend a small amount of
+                    time learning about new things with a group of people with
+                    same interests!
+                </p>
+                <button onClick={() => navigate("/dashboard/learning-circle")}>
+                    Create/Join Learning Circles
+                </button>
+            </div>
+            ;
+        </>
+    );
+};
+
+export default LcHero;
